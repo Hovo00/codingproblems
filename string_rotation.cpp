133271a1327ba5d452423b067aad683a4e24af03
Hovo00 <90268646+Hovo00@users.noreply.github.com>
2022-09-04 00:06:20 +0300

    Create string_rotation.cpp

---
+++ string_rotation.cpp
@@ -0,0 +1,8 @@
+bool isRotation(const std::string& s1,const std::string s2) {
+	if (s1.size() == s2.size() && s1.size() > 0) {
+	
+	std::string s1s2 = s1 + s2;
+		return isSubstring(slsl, s2);
+	}
+	return false;
+}
